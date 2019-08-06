eddf9e74cc58e467ef8ad048adb87c01e1468a21
Matthew <mfarmer5102@gmail.com>
2019-08-06 15:04:44 -0400

    Redesigned UI; fixed bug where friendlist was being overwritten after first run due to not putting '===' in if statement

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,16 +2,16 @@ module.exports = [
     {
         "name": "Mark",
         "photo": "https://via.placeholder.com/150",
-        "scores": [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+        "scores": [5, 3, 4, 1, 1 ,2, 4, 5, 2, 5]
     },
     {
         "name": "John",
         "photo": "https://via.placeholder.com/150",
-        "scores": [2, 3, 4, 5, 1, 2, 3, 4, 5, 1]
+        "scores": [1, 5, 3, 5, 5, 2, 4, 3, 5, 1]
     },
     {
         "name": "Michael",
         "photo": "https://via.placeholder.com/150",
-        "scores": [3, 4, 5, 1, 2, 3, 4, 5, 1, 2]
+        "scores": [3, 5, 5, 4, 2, 1, 1, 3, 4, 5]
     }
 ]
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path')
 
 //Express->App and Port Definition
 const app = express()
-let port = process.env.PORT|| 3000
+let port = process.env.PORT || 3000
 
 //Allow express to handle post requests
 app.use(express.urlencoded({ extended: true }));
@@ -13,26 +13,29 @@ app.use(express.json());
 //Friend List Import
 var friendList = require('./app/data/friends.js')
 
+//Express folder definitions
+app.use(express.static(__dirname + '/app/public/'));
+
 //#############################################################################
 
 //HTML ROUTES
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+app.get('/', function (req, res) {
+    res.sendFile('home.html', { root: __dirname + `/app/public/` });
 })
 
-app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+app.get('/survey', function (req, res) {
+    res.sendFile('survey.html', { root: __dirname + `/app/public/` });
 })
 
 //API ROUTES
-app.get('/api/friends', function(req, res) {
+app.get('/api/friends', function (req, res) {
     res.json(friendList)
 })
 
-app.post('/api/friends', function(req, res) {
+app.post('/api/friends', function (req, res) {
     let yourScores = (Object.values(req.body))
     console.log(yourScores)
-    res.send('Best match: ' + findBestMatch(yourScores))
+    res.send(findBestMatch(yourScores))
 })
 
 //#############################################################################
@@ -45,43 +48,66 @@ app.listen(port, function () {
 //#############################################################################
 
 function findBestMatch(yourScores) {
-        //Establish an empty result object
-        let resultObj = {}
 
-        //Calculate difference between your scores and everyone else's
-        for (i=0; i < friendList.length; i++) {
-            let totalDifference = 0;
-            let theirScores = (friendList[i].scores)
-            for (j=0; j < theirScores.length; j++) {
-                totalDifference += Math.abs(yourScores[j] - theirScores[j])
-            }
-    
-            let personName = friendList[i].name
-            let personDiff = totalDifference
-            console.log(`${personName}: ${personDiff}`)
-    
-            //Add that person to the result object with their score
-            resultObj[personName] = personDiff
+    console.log(` ************* SHOWING FULL LIST OF FRIENDS ************* `)
+    for (i = 0; i < friendList.length; i++) {
+        console.log(friendList[i].name)
+    }
+
+    console.log(` ************* FINDING BEST MATCH ************* `)
+    //Establish an empty result object
+    let resultObj = {}
+
+    //Calculate difference between your scores and everyone else's
+    for (i = 0; i < friendList.length; i++) {
+        let totalDifference = 0;
+        let theirScores = (friendList[i].scores)
+        for (j = 0; j < theirScores.length; j++) {
+            totalDifference += Math.abs(yourScores[j] - theirScores[j])
         }
-    
-        //Put the scores into an array
-        let scores = Object.values(resultObj)
-    
-        //Find the lowest value
-        let indexOfLeastDifferent;
-        let lowestValue;
-        for (i=0; i < scores.length; i++) {
-            if (scores[i] < lowestValue || lowestValue === undefined) {
-                lowestValue = scores[i]
-                indexOfLeastDifferent = i
-            }
+
+        let personName = friendList[i].name
+        let personDiff = totalDifference
+        console.log(`${personName}: ${personDiff}`)
+
+        //Add that person to the result object with their score
+        resultObj[personName] = personDiff
+    }
+
+    //Put the scores into an array
+    let scores = Object.values(resultObj)
+
+    //Find the lowest value
+    let indexOfLeastDifferent;
+    let lowestValue;
+    for (i = 0; i < scores.length; i++) {
+        if (scores[i] < lowestValue || lowestValue === undefined) {
+            console.log(scores[i])
+            lowestValue = scores[i]
+            indexOfLeastDifferent = i
         }
-        console.log('Index of Least Different:' + indexOfLeastDifferent)
-    
-        //Match index of lowest score to index of best match's name
-        let names = (Object.keys(resultObj))
-        let bestMatch= (names[indexOfLeastDifferent])
-    
-        //Return name of the best match
-        return bestMatch
+    }
+    console.log('Index of Least Different:' + indexOfLeastDifferent)
+
+    //Match index of lowest score to index of best match's name
+    let names = (Object.keys(resultObj))
+    let bestMatchName = (names[indexOfLeastDifferent])
+
+    //Find the photo of the best match
+    let bestMatchPhoto = '';
+    for (i = 0; i < friendList.length; i++) {
+        if (friendList[i].name === bestMatchName) {
+            bestMatchPhoto = friendList[i].photo
+        }
+    }
+
+    //Create a response object contaning the name and photo of the best match
+    let bestMatchObject = {
+        name: bestMatchName,
+        photo: bestMatchPhoto
+    }
+
+    //Return the best match object
+    console.log(bestMatchObject)
+    return bestMatchObject
 }
