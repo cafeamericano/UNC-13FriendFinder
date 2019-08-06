a73d200b756dfa02d8d851a86c7ca7afa07dbfeb
Matthew <mfarmer5102@gmail.com>
2019-08-06 15:28:21 -0400

    Changed theme and questions for application

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 module.exports = [
     {
-        "name": "Mark",
-        "photo": "https://via.placeholder.com/150",
-        "scores": [5, 3, 4, 1, 1 ,2, 4, 5, 2, 5]
+        "name": "William the Dog",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/4/47/Golden_retriever.jpg",
+        "scores": [5, 3, 5, 5, 1, 5, 1, 5, 5, 5]
     },
     {
-        "name": "John",
-        "photo": "https://via.placeholder.com/150",
-        "scores": [1, 5, 3, 5, 5, 2, 4, 3, 5, 1]
+        "name": "Christopher the Bunny",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/c/c7/Honey_bunny.jpg",
+        "scores": [2, 1, 5, 3, 5, 5, 1, 1, 4, 2]
     },
     {
-        "name": "Michael",
-        "photo": "https://via.placeholder.com/150",
-        "scores": [3, 5, 5, 4, 2, 1, 1, 3, 4, 5]
+        "name": "Edward the Cat",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/d/dc/Nos_kota.jpg",
+        "scores": [1, 5, 1, 2, 3, 1, 5, 1, 2, 1]
     }
 ]
